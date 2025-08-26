c8922e1cf34b6a8f0243428e69075b83c123e1dc
DonRaijin <donbounleuang@gmail.com>
2025-08-26 11:49:13 -0700

    Add files via upload

---
+++ ForLoopsPractice.java
@@ -0,0 +1,18 @@
+// For Loop Practice
+// db 8/26/25
+
+public class ForLoopsPractice {
+    public static void main(String[] args) {
+
+
+// A loop must have a loop
+// control variable
+// The loops control variable must
+// be initialized, checked, and changed
+
+for(int i = 1; i <= 100; i++) {
+    System.out.println(i);
+        }
+
+    }
+}
