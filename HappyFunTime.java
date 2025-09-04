94e0659875df5bb59595c16b241dbfa69d3b4d08
DonRaijin <donbounleuang@gmail.com>
2025-09-04 10:47:15 -0700

    Add files via upload

---
+++ HappyFunTime.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class HappyFunTime {
+
+    public static void main(String[] args) {
+
+        String[] cars = {"NSX", "Skyline R34", "RX7"};
+
+        for(String car : cars) {
+            System.out.println(car);
+        }
+
+        }
+
+    }
